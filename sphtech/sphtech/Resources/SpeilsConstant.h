d3b5cc932900f8b6a29d8096b8b45515eee9b1d6
Apolonio <michaelugale626@gmail.com>
2019-01-14 19:31:05 +0800

    resources
    - created constant and splies file container
    
    - integrated custom fonts

---
+++ sphtech/sphtech/Resources/SpeilsConstant.h
@@ -0,0 +1,22 @@
+//
+//  SpeilsConstant.h
+//  sphtech
+//
+//  Created by Michael Ugale on 1/14/19.
+//  Copyright © 2019 Michael Ugale. All rights reserved.
+//
+
+#ifndef SpeilsConstant_h
+#define SpeilsConstant_h
+
+#define NO_INTERNET_CONNECTION              @"No internet connection.  Please try again."
+#define ISSUE_ACCESS_TOKEN                  @"Authentication failed."
+#define SESSION_EXPIRED                     @"Session Expired"
+#define SESSION_EXPIRED_MESSAGE             @"Your session has expired. Please log in again."
+#define SOMETHING_WENT_WRONG                @"Something went wrong. Please try again later."
+#define CONNECTION_ERROR                    @"Connection Error"
+
+#define BUTTON_OK                           @"Ok"
+
+
+#endif /* SpeilsConstant_h */
