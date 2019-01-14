a5eaed29cf23393e66bf44c92d851db4ea497b4a
Apolonio <michaelugale626@gmail.com>
2019-01-14 19:38:08 +0800

    connection check
    - integrated 3rd party connection check or reachability

---
+++ sphtech/sphtech/Manager/API/ServerManager.h
@@ -0,0 +1,15 @@
+//
+//  ServerManager.h
+//  sphtech
+//
+//  Created by Michael Ugale on 1/14/19.
+//  Copyright © 2019 Michael Ugale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
+#import "Reachability.h"
+
+@interface ServerManager : NSObject
+
+@end
---
+++ sphtech/sphtech/Manager/API/ServerManager.m
@@ -0,0 +1,13 @@
+//
+//  ServerManager.m
+//  sphtech
+//
+//  Created by Michael Ugale on 1/14/19.
+//  Copyright © 2019 Michael Ugale. All rights reserved.
+//
+
+#import "ServerManager.h"
+
+@implementation ServerManager
+
+@end
